770d0e96a6679dd986422d9985614f5fb53af2ca
Shangxin Du <shdu@cisco.com>
2022-05-12 11:44:57 -0700

    empty files

--- task03_policy/main.tf
+++
@@ -1,9 +0,0 @@
-module "policy" {
-  source      = "../modules/policy"
-  ndfc        = var.ndfc
-  fabric_name = var.fabric_name
-  inventory   = var.inventory
-  templates   = var.templates
-  policies    = var.policies
-}
-
