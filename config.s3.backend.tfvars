19c90150bf9001bf9ccf8239fd227b3aac288f28
Shangxin Du <shdu@cisco.com>
2023-01-13 20:56:47 -0800

    update to use backend config file

---
+++ config.s3.backend.tfvars
@@ -0,0 +1,4 @@
+bucket         = "tf-backend-pod06"
+key            = "terraform.tfstate"
+region         = "us-west-2"
+dynamodb_table = "terraform-locks"
--- main.tf
+++ main.tf
@@ -6,10 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "tf-backend-pod06"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
+
   }
 }
 
